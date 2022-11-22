8436eca9b78c4e692dd9cfb78aa8e54d2a3688ab
Simic Ivana, GHR-OSA-NEX-24 <Ivana.Simic@swisscom.com>
2022-11-22 11:28:55 +0100

    created project

---
+++ src/main/java/ch/zli/m223/service/EntryService.java
@@ -0,0 +1,43 @@
+package ch.zli.m223.service;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import ch.zli.m223.model.Entry;
+
+@ApplicationScoped
+public class EntryService {
+    @Inject
+    private EntityManager entityManager;
+
+    @Transactional
+    public Entry createEntry(Entry entry) {
+        entityManager.persist(entry);
+        return entry;
+    }
+
+    public List<Entry> findAll() {
+        var query = entityManager.createQuery("FROM Entry", Entry.class);
+        return query.getResultList();
+    }
+
+    public void updateEntity(Entry entry) {
+    }
+
+    //delete
+    @Transactional
+    public void deleteEntry(Long id) {
+      var entity = entityManager.find(Entry.class, id);
+      entityManager.remove(entity);
+    }
+
+
+    @Transactional
+    public Entry updateEntry(Long id, Entry entry) {
+      return entityManager.merge(entry);
+    }
+}
