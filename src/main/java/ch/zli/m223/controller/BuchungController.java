b4ecac7f2e56cc37de907e0373a815e383af0379
Simic Ivana, GHR-OSA-NEX-24 <Ivana.Simic@swisscom.com>
2022-11-23 08:36:54 +0100

    BuchungController and BuchungService implemented

--- src/main/java/ch/zli/m223/controller/BuchungController.java
+++ src/main/java/ch/zli/m223/controller/BuchungController.java
@@ -1,5 +1,67 @@
 package ch.zli.m223.controller;
 
+import java.util.List;
+
+
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import ch.zli.m223.model.Buchung;
+import ch.zli.m223.service.BuchungService;
+
+/*
+ * @author Ivana Simic
+ * @date 22.11.2022
+ * Der Controller für die Buchung
+ */
+
+
+@Path ("/buchung")
 public class BuchungController {
-  
+  @Inject
+  BuchungService buchungService;
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public List<Buchung> getBuchungen() {
+      return buchungService.findAll();
+  }
+  @Path("/{id}")
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Buchung getOneBuchung(@PathParam("id") Long id) {
+      if (id >= 1) {
+          return buchungService.findId(id);
+      } else {
+          throw new IllegalArgumentException("Id should be greater than 1");
+      }
+  }
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Buchung create(Buchung buchung) {
+      return buchungService.create(buchung);
+  }
+  @Path("/{id}")
+  @PUT
+  public Buchung update(@PathParam("id") Long id, Buchung buchung) {
+      if (id >= 1) {
+          return buchungService.update(id, buchung);
+      } else {
+          throw new IllegalArgumentException("Id should be greater than 1");
+      }
+  }
+  @Path("/{id}")
+  @DELETE
+  public void delete(@PathParam("id") Long id) {
+      buchungService.delete(id);
+  }
 }
--- src/main/java/ch/zli/m223/service/BuchungService.java
+++ src/main/java/ch/zli/m223/service/BuchungService.java
@@ -1,5 +1,46 @@
 package ch.zli.m223.service;
 
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import ch.zli.m223.model.Buchung;
+
+import javax.transaction.Transactional;
+
+/*
+ * @author Ivana Simic
+ * @date 22.11.2022
+ * Der Service für die Buchung
+ */
+
+ @ApplicationScoped
 public class BuchungService {
+  @Inject
+  EntityManager entityManager;
+  public List<Buchung> findAll() {
+      var query = entityManager.createQuery("FROM Buchung", Buchung.class);
+      return query.getResultList();
+  }
+  public Buchung findId(Long id){
+      var buchung = entityManager.find(Buchung.class, id);
+      return buchung;
+  }
+  @Transactional
+  public Buchung create(Buchung buchung) {
+      entityManager.persist(buchung);
+      return buchung;
+  }
+  @Transactional
+  public Buchung update(Long id, Buchung buchung) {
+      return entityManager.merge(buchung);
+  }
+  @Transactional
+  public void delete(Long id) {
+      var buchung = entityManager.find(Buchung.class, id);
+      entityManager.remove(buchung);
+  }
   
 }
